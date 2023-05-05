5d2821dbb1fa8c5447bda70f0ab67a8dd8230188
brandon-bennington <brandon.bennington@codeimmersives.com>
2023-05-05 20:50:09 -0300

    trying to find issue still

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <h1>Restaurant Name</h1>
+      <h1>Oceanico Sushi Bar</h1>
       {/* Add your logo image here */}
       {/* <img src="logo.png" alt="Restaurant Logo" /> */}
     </header>
--- src/pages/EmployeeDashboard/EmployeeDashboard.js
+++ src/pages/EmployeeDashboard/EmployeeDashboard.js
@@ -3,26 +3,12 @@ import axios from 'axios';
 import './EmployeeDashboard.css';
 import MenuManagement from './MenuManagement';
 
-
 function EmployeeDashboard() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [registerForm, setRegisterForm] = useState({ username: '', password: '', usertype: 'employee' });
+  const [token, setToken] = useState('');
   const [loginForm, setLoginForm] = useState({ username: '', password: '' });
+  const [registerForm, setRegisterForm] = useState({ username: '', password: '', usertype: 'employee' });
   const [error, setError] = useState(null);
-
-  const handleRegisterSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`http://localhost:5001/api/users/register`, registerForm);
-      if (response.status === 201) {
-        setError(null);
-        setLoginForm({ username: registerForm.username, password: registerForm.password });
-        setRegisterForm({ username: '', password: '', usertype: 'employee' });
-      }
-    } catch (err) {
-      setError(err.response.data.message);
-    }
-  };
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
@@ -31,6 +17,25 @@ function EmployeeDashboard() {
       if (response.status === 200) {
         setError(null);
         setLoggedIn(true);
+        setToken(response.data.token);
+        console.log('Logged in!'); 
+      }
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+
+  const handleLoginInputChange = (e) => {
+    setLoginForm({ ...loginForm, [e.target.id]: e.target.value });
+  };
+
+  const handleRegisterSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:5001/api/users/register`, registerForm);
+      if (response.status === 201) {
+        setError(null);
+        setRegisterForm({ username: '', password: '', usertype: 'employee' });
       }
     } catch (err) {
       setError(err.response.data.message);
@@ -41,16 +46,20 @@ function EmployeeDashboard() {
     setRegisterForm({ ...registerForm, [e.target.id]: e.target.value });
   };
 
-  const handleLoginInputChange = (e) => {
-    setLoginForm({ ...loginForm, [e.target.id]: e.target.value });
+  const handleLogout = () => {
+    setLoggedIn(false);
   };
 
-const menuManagementInterface = (
-  <div>
-    <h2>Menu Management</h2>
-    <MenuManagement />
-  </div>
-);
+  const menuManagementInterface = (
+    <div>
+      <h2>Menu Management</h2>
+      <MenuManagement token={token} />
+      <button className="btn btn-secondary mt-3" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
+  console.log('loggedIn', loggedIn); // Temporary console log
 
   return (
     <div className="container">
@@ -84,16 +93,42 @@ const menuManagementInterface = (
                 Register
               </button>
             </form>
+          </div>
+          <div className="col-md-6">
+            <h2 className="text-center">Login</h2>
+            <form onSubmit={handleLoginSubmit} className="p-3">
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={loginForm.username}
+                  onChange={handleLoginInputChange}
+                  className="form-control"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      value={loginForm.password}
+                      onChange={handleLoginInputChange}
+                      className="form-control"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Login
+                  </button>
+                </form>
+              </div>
+            </div>
+          ) : (
+            <div>{menuManagementInterface}</div>
+          )}
+          {error && <div className="error">{error}</div>}
+        </div>
+        );
+      }
       
-      </div>
-    </div>
-  ) : (
-    <div>{menuManagementInterface}</div>
-  )}
-  {error && <div className="error">{error}</div>}
-</div>
-               
-);
-}
-
-export default EmployeeDashboard;
+      export default EmployeeDashboard;
